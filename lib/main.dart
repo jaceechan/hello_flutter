2f3c2b765e80ac464a95cad08c14ac4e0077ca5d
jacee.chan <jaceechan@foxmail.com>
2022-07-06 23:42:45 +0800

    feat: 不规则导航栏

--- lib/main.dart
+++ lib/main.dart
@@ -17,33 +17,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo'),
+      home: const CustomBottomPage(title: 'Flutter Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -68,12 +50,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         bottomNavigationBar: BottomNavigationBar(
           items: [
@@ -100,8 +76,85 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class NewRoute extends StatelessWidget {
-  const NewRoute({Key? key, required this.text}) : super(key: key);
+class CustomBottomPage extends StatefulWidget {
+  const CustomBottomPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<CustomBottomPage> createState() => _CustomBottomPageState();
+}
+
+class _CustomBottomPageState extends State<CustomBottomPage> {
+  int _currentIndex = 0;
+  final _bottomIconColor = Colors.indigo;
+  List<Widget> list = [];
+
+  @override
+  void initState() {
+    list
+      ..add(const HomePage())
+      ..add(const MinePage());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          var result = Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return const NewPage(
+                  // 路由传参
+                  text: 'NewPage');
+            }),
+          );
+          print("路由返回值: $result");
+        },
+        tooltip: 'Add',
+        backgroundColor: _bottomIconColor,
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        color: _bottomIconColor,
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 0;
+                  });
+                },
+                icon: const Icon(
+                  Icons.home,
+                  color: Colors.white,
+                )),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    _currentIndex = 1;
+                  });
+                },
+                icon: const Icon(
+                  Icons.assignment_ind,
+                  color: Colors.white,
+                )),
+          ],
+        ),
+      ),
+      body: list[_currentIndex],
+    );
+  }
+}
+
+class NewPage extends StatelessWidget {
+  const NewPage({Key? key, required this.text}) : super(key: key);
 
   final String text;
 
@@ -109,7 +162,7 @@ class NewRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("New route"),
+        title: const Text("New page"),
       ),
       body: Center(
           child: Column(
