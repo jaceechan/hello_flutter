cb0f97dd3df6e6b423fbae3dfa89ba0d1270759e
jaceechan <jaceeqq@163.com>
2022-07-30 00:19:59 +0800

    feat: 贝塞尔曲线切割进阶

--- lib/pages/custom_clipper.dart
+++ lib/pages/custom_clipper.dart
@@ -12,16 +12,22 @@ class CustomClipperDemo extends StatelessWidget {
       body: Column(
         children: <Widget>[
           ClipPath(
-            clipper: BottomClipper(),
+            clipper: TopClipper(),
             child: Container(color: Colors.amber, height: 200.0),
-          )
+          ),
+          Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: ClipPath(
+                clipper: BottomClipper(),
+                child: Container(color: Colors.purple, height: 200.0),
+              ))
         ],
       ),
     );
   }
 }
 
-class BottomClipper extends CustomClipper<Path> {
+class TopClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = Path();
@@ -35,6 +41,36 @@ class BottomClipper extends CustomClipper<Path> {
         fisrtEndPoint.dx, fisrtEndPoint.dy);
 
     path.lineTo(size.width, size.height - 60);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+class BottomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height - 20);
+
+    var fisrtControlPoint = Offset(size.width / 4, size.height);
+    var fisrtEndPoint = Offset(size.width / 2.25, size.height - 30);
+
+    path.quadraticBezierTo(fisrtControlPoint.dx, fisrtControlPoint.dy,
+        fisrtEndPoint.dx, fisrtEndPoint.dy);
+
+    var secondControlPoint = Offset(size.width * 0.75, size.height - 80);
+    var secondEndPoint = Offset(size.width, size.height - 40);
+
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    path.lineTo(size.width, size.height - 40);
     path.lineTo(size.width, 0);
     return path;
   }
